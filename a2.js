c4ffab2e956b588f77bd6399e2649cac27c14ab2
emadchy96 <echowdhury7@myseneca.ca>
2024-05-31 01:20:10 -0400

    Update a2.js

--- a2.js
+++ a2.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB700 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Emad Chowdhury Student ID: 161788237 Date: 30 May 2024
+*
+********************************************************************************/
+
 const collegeData = require('./modules/collegeData.js');
 
 collegeData.initialize()
